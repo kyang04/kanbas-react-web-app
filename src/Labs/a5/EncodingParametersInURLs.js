72c792197f33a98fcf5f94c5ea91f401ad8cb745
Kevin Yang <kevingyang72@gmail.com>
2023-11-20 17:53:50 -0500

    changing encoding

--- src/Labs/a5/EncodingParametersInURLs.js
+++ src/Labs/a5/EncodingParametersInURLs.js
@@ -14,9 +14,7 @@ function EncodingParametersInURLs() {
     useEffect(() => {
         fetchWelcome();
     }, []);
-
     const [result, setResult] = useState(0);
-
     const fetchSum = async (a, b) => {
         const response = await axios.get(`${API_BASE}/a5/add/${a}/${b}`);
         setResult(response.data);
@@ -29,7 +27,29 @@ function EncodingParametersInURLs() {
 
     return (
         <div>
-            {/* ... Your existing JSX ... */}
+            <h3>Encoding Parameters In URLs</h3>
+            <h4>Integrating React with APIs</h4>
+            <h5>Fetching Welcome</h5>
+            <h6>{welcome}</h6>
+            <h4>Calculator</h4>
+            <input
+                onChange={(e) => setA(e.target.value)}
+                className="form-control" type="number" value={a}/>
+            <input
+                onChange={(e) => setB(e.target.value)}
+                className="form-control" type="number" value={b}/>
+            <input value={result}
+                   className="form-control mb-2" type="number" readOnly
+            />
+            <h3>Fetch Result</h3>
+            <button onClick={() => fetchSum(a, b)}
+                    className="btn btn-primary mb-2  w-100" >
+                Fetch Sum of {a} + {b}
+            </button>
+            <button onClick={() => fetchSubtraction(a, b)}
+                    className="btn btn-danger me-2 w-100" >
+                Fetch Substraction of {a} - {b}
+            </button>
             <h3>Path Parameters</h3>
             <a
                 href={`${API_BASE}/a5/add/${a}/${b}`}
@@ -39,7 +59,7 @@ function EncodingParametersInURLs() {
             <a
                 href={`${API_BASE}/a5/subtract/${a}/${b}`}
                 className="btn btn-danger">
-                Subtract {a} - {b}
+                Substract {a} - {b}
             </a>
             <h3>Query Parameters</h3>
             <a
@@ -50,10 +70,10 @@ function EncodingParametersInURLs() {
             <a
                 href={`${API_BASE}/a5/calculator?operation=subtract&a=${a}&b=${b}`}
                 className="btn btn-danger">
-                Subtract {a} - {b}
+                Substract {a} - {b}
             </a>
         </div>
     );
 }
-
 export default EncodingParametersInURLs;
+
