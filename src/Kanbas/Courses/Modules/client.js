ce814c1b09c5b6af467e0c9b822733104fc025f7
Kevin Yang <kevingyang72@gmail.com>
2023-11-20 20:30:49 -0500

    fix mods

--- src/Kanbas/Courses/Modules/client.js
+++ src/Kanbas/Courses/Modules/client.js
@@ -1,7 +1,7 @@
 import axios from "axios";
-const COURSES_URL = `${API_BASE}/courses`;
+const COURSES_URL = `${API_BASE}/api/courses`;
 const API_BASE = process.env.REACT_APP_API_BASE;
-const MODULES_URL = `${API_BASE}/modules`;
+const MODULES_URL = `${API_BASE}/api/modules`;
 export const deleteModule = async (moduleId) => {
     const response = await axios
         .delete(`${MODULES_URL}/${moduleId}`);
